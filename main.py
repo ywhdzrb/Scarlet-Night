51dd130a0887765cd8568cb1b1433091fc7bff39
ywhdzrb <intmainreturn@outlook.com>
2024-11-30 19:32:03 +0800

    -

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import pygame as pg
-from log import log
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+import sys
 
-class WIN:
-    def __init__(self):
-        pg.init()
-        self.screen = pg.display.set_mode((800, 500))
-        self.clock = pg.time.Clock()
-        self.running = True
-        self.fps = 60
-        log("Init")
-    
-    def run(self):
-        while self.running:
-            self.clock.tick(self.fps)
-            self.screen.fill((0, 0, 0))
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.running = False
-            pg.display.flip()
-        pg.quit()
-        log("Quit")
+width = 800
+height = 500
 
-if __name__ == "__main__":
-    win = WIN()
-    win.run()
+def br_show():
+    global win
+    label = QLabel(win)
+    pixmap = QPixmap("assets/background/image/br.png")
+    pixmap_resized = pixmap.scaled(width, height, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+    label.setPixmap(pixmap_resized)
+    # 调整图片
+    label.setScaledContents(True)
+
+    label.show()
+
+def main():
+    global win
+    app = QApplication(sys.argv)
+    win = QWidget()
+    win.setGeometry(width, height, width, height)
+    win.setWindowTitle("Scarlet Night")  
+    win.setFixedSize(800, 500)
+    win.show()
+
+    br_show()
+
+    sys.exit(app.exec_())
+
+main()
